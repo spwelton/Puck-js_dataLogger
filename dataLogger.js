3ff707d4c4820ac6a58e9c08435f80555ac87b86
spwelton <48292331+spwelton@users.noreply.github.com>
2020-03-06 22:04:00 -0500

    Log battery percentage as well

--- dataLogger.js
+++ dataLogger.js
@@ -1,30 +1,34 @@
-var log = new Float32Array(100); // our logged data
+var logTemperature = new Float32Array(100); // our logged temperature data
+var logBatteryPercentage = new Float32Array(100); // our logged battery % data
 var logIndex = 0; // index of last logged data item
 var timePeriod = 600*1000; // every 10 minutes
 var lastReadingTime; // time of last reading
 
 // Store data into RAM
-function storeMyData(data) {
+function storeMyData(temperature, batteryPercentage) {
   logIndex++;
-  if (logIndex>=log.length) logIndex=0;
-  log[logIndex] = data;
+  if (logIndex>=logTemperature.length) logIndex=0;
+  logTemperature[logIndex] = temperature;
+  logBatteryPercentage[logIndex] = batteryPercentage;
 }
 
 // Get Data and store it in RAM
 function getData() {
   LED3.write(1);
-  var data = E.getTemperature();
-  storeMyData(data);
+  var temperature = E.getTemperature();
+  var batteryPercentage = Puck.getBatteryPercentage();
+  storeMyData(temperature, batteryPercentage);
   lastReadingTime = Date.now();
   LED3.write(0);
 }
 
 // Dump our data in a human-readable format
 function exportData() {
-  for (var i=1;i<=log.length;i++) {
-    var time = new Date(lastReadingTime - (log.length-i)*timePeriod);
-    var data = log[(i+logIndex)%log.length];
-    console.log(time.toString()+"\t"+data);
+  for (var i=1;i<=logTemperature.length;i++) {
+    var time = new Date(lastReadingTime - (logTemperature.length-i)*timePeriod);
+    var temperature = logTemperature[(i+logIndex)%logTemperature.length];
+    var batteryPercentage = logBatteryPercentage[(i+logIndex)%logBatteryPercentage.length];
+    console.log(time.toString()+"\t"+temperature+"\t"+batteryPercentage);
   }
 }
 
